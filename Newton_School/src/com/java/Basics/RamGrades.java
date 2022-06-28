6aa45e0888e6d2bf2a89b46b512c7ed76f66325d
KunalDhruw <kunaldhruw1998@gmail.com>
2022-06-28 20:12:28 +0530

    Added New Class file Ram Grades Question

--- Newton_School/src/com/java/Basics/RamGrades.java
+++ Newton_School/src/com/java/Basics/RamGrades.java
@@ -3,14 +3,14 @@ package com.java.Basics;
 import java.util.Scanner;
 
 public class RamGrades {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
         int d = sc.nextInt();
-        int sum = a+b+c+d;
-        int percentage = sum /4 ;
+        int sum = a + b + c + d;
+        int percentage = sum / 4;
         System.out.println(percentage);
     }
 }
